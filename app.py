5a1780d477d68add980cb33b2cd44f253bcb4013
Eise Olusegun William <Williamseisenherz@gmail.com>
2023-09-09 15:26:13 +0100

    comments

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = FastAPI()
 
 @app.get("/api")
 def get_info(slack_name: str, track: str):
+    # get time and format as day in full
     day = datetime.utcnow().strftime("%A")
+    # get time and format in utc
     time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    # build dict of info
     info = {
         "slack_name": slack_name,
         "track": track,
